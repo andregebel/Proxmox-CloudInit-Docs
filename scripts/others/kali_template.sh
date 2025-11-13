d6e7693f49c0a40d31ccce20067c4874a5126b7b
Andre <150909409+andregebel@users.noreply.github.com>
2025-11-13 17:02:29 +0100

    Add script to create Kali Linux VM template

--- scripts/others/kali_template.sh
+++ scripts/others/kali_template.sh
@@ -1,1 +1,26 @@
+#! /bin/bash
 
+VMID=8501
+STORAGE=ssd1
+
+apt update -y && apt install libguestfs-tools -y
+apt install unar -y
+cd /var/lib/vz/template/iso
+set -x
+rm -f kali-linux-2025.3-qemu-amd64.qcow2
+wget -q https://eu.mirror.ionos.com/linux/distributions/kali/kali-images/current/kali-linux-2025.3-qemu-amd64.7z
+unar kali-linux-2025.3-qemu-amd64.7z
+qemu-img resize kali-linux-2025.3-qemu-amd64.qcow2 32G
+ qm destroy $VMID
+ qm create $VMID --name "kali-linux-2025.3-template" --ostype l26 \
+    --memory 4092 --balloon 0 \
+    --agent 1 \
+    --cpu x86-64-v2-AES --cores 2 --numa 1 \
+    --net0 virtio,bridge=vmbr0,mtu=1
+ qm importdisk $VMID kali-linux-2025.3-qemu-amd64.qcow2 $STORAGE
+ qm set $VMID --scsihw virtio-scsi-pci --virtio0 $STORAGE:vm-$VMID-disk-1,discard=on
+ qm set $VMID --boot order=virtio0
+ qm set $VMID --tags kali-template,kali-2025.3,
+ qm set $VMID --sshkeys ~/.ssh/authorized_keys
+ qm set $VMID --ipconfig0 ip=dhcp
+ qm template $VMID
